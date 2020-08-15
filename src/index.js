f8b2e929c6318e1fd34d48affaf684ab87edd0ed
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-08-15 16:07:16 +0300

    начал делать домашнее задание

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles.css';
+import itemTemplate from './templates/item.hbs';
+import refs from './js/refs';
+
+function fetchCountries() {
+  const url = `https://restcountries.eu/rest/v2/`;
+  return fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      //   console.log(itemTemplate(data));
+      refs.items.innerHTML = itemTemplate(data);
+    });
+}
+fetchCountries();
+console.log(refs);
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+  form: document.querySelector('.form'),
+  input: document.querySelector('.input'),
+  button: document.querySelector('.button'),
+  items: document.querySelector('.countries'),
+};
+module.exports = refs;
